5829ed891a14477f7e4be0d39543131927aea111
Brendan Cox <bcox@elotl.co>
2017-11-15 20:52:54 -0800

    create more complex hello_world and build as shared lib

--- examples/hello_world_cpp/hello_world.cc
+++ examples/hello_world_cpp/hello_world.cc
@@ -18,11 +18,18 @@
  */
 
 #include <fluent-bit.h>
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <unistd.h>
+
+using namespace std;
 
 int main()
 {
     int i;
     int n;
+    int ct;
     int ret;
     char tmp[256];
     flb_ctx_t *ctx;
@@ -32,28 +39,62 @@ int main()
     /* Initialize library */
     ctx = flb_create();
 
-    in_ffd = flb_input(ctx, (char *) "lib", NULL);
-    flb_input_set(ctx, in_ffd, (char *) "test", NULL);
+    ofstream myfile;
+
+    myfile.open("/mylog.txt");
+    for (i = 0; i < 100; i++) {
+        myfile << "Log line" << i << endl;
+    }
+
+    in_ffd = flb_input(ctx, (char *) "tail", NULL);
+    int ret_val = flb_input_set(ctx, in_ffd,
+                                "path", "/mylog.txt",
+                                "tag", "debug.55",
+                                NULL);
+    if (ret_val != 0) {
+        throw runtime_error("error setting input param");
+    }
 
     out_ffd = flb_output(ctx, (char *) "stdout", NULL);
     flb_output_set(ctx, out_ffd, (char *) "test", NULL);
 
+    // out_ffd = flb_output(ctx, (char *) "forward", NULL);
+    // ret_val = flb_output_set(ctx, out_ffd,
+    //                          "host", "10.0.9.61",
+    //                          "hostname", "osv.local",
+    //                          "port", "24224",
+    //                          "time_as_integer", "true",
+    //                          "tls", "on",
+    //                          "tls.verify", "off",
+    //                          "Shared_Key", "secret",
+    //                          NULL);
+    // if (ret_val != 0) {
+    //     throw runtime_error("error setting input param");
+    // }
+    // flb_output_set(ctx, out_ffd, (char *) "test", NULL);
+
     /* Start the background worker */
     flb_start(ctx);
 
-    /* Push some data */
-    for (i = 0; i < 100; i++) {
-        n = snprintf(tmp, sizeof(tmp) - 1,
-                     "[%lu, {\"key\": \"val %i\"}]",
-                     time(NULL), i);
-        printf("%s\n", tmp);
-        flb_lib_push(ctx, in_ffd, tmp, n);
+    ct = 100;
+    cout << "got to here\n";
+    for (;;) {
+        for (int j = 0; j < 10; j++) {
+            myfile << "Log line" << ct << endl;
+            cout << "Writing " << ct << endl;
+            ++ct;
+            sleep(1);
+        }
+        myfile << "Wrapped" << ct << endl;
+        myfile.flush();
     }
+    myfile.close();
 
     flb_stop(ctx);
 
     /* Release Resources */
     flb_destroy(ctx);
+
 
     return 0;
 }
